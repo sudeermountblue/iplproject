bf14ab9df21c87a19fc39fa66dbf16acac9bf47e
sudeer <sudeermountblue@gmail.com>
2022-05-22 12:31:01 +0530

    retrieved the data of each teams extra runs for 2016 season

--- Question3.java
+++ Question3.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Question3 {
@@ -13,6 +14,7 @@ public class Question3 {
     public static void main(String[] args) {
 
         String path="/home/sudheer/Downloads/archive/matches.csv";
+        String deliveriesFilePath="/home/sudheer/Downloads/archive/deliveries.csv";
         String line="";
 
         try {
@@ -24,8 +26,32 @@ public class Question3 {
                     matchIds.add(values[0]);
                 }
             }
-            System.out.println(matchIds);
+            //System.out.println(matchIds);
 
+            BufferedReader br1=new BufferedReader(new FileReader(deliveriesFilePath));
+
+            HashMap<String,Integer> teamExtraRuns=new HashMap();
+            while((line = br1.readLine()) !=null) {
+
+                String[] values=line.split(",");
+                if (matchIds.contains(values[0])){
+                    if(teamExtraRuns.containsKey(values[3])) {
+                        int count=teamExtraRuns.get(values[3]);
+                        teamExtraRuns.put(values[3],count+Integer.valueOf(values[16]));
+
+                    }
+
+                    else {
+                        teamExtraRuns.put(values[3], Integer.valueOf(values[16]));
+                    }
+
+                }
+
+
+
+            }
+            System.out.println("For the year 2016 the extra runs conceded per team");
+            System.out.println(teamExtraRuns);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }catch (IOException e) {
